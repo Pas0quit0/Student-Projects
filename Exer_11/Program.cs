21a12198e978cf2a4cecab44ae8942c78df04b1b
Marcos Melo <31050100+Pas0quit0@users.noreply.github.com>
2018-05-09 16:03:57 -0300

    Add files via upload

---
+++ Exer_11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string oper;
+            ToInt values = new ToInt();
+
+            Console.WriteLine("Digite dois valores inteiros separados por um operador +, -, * ou /");
+            oper = Console.ReadLine();
+
+            values.Identify(oper);
+
+        }
+    }
+}
---
+++ Exer_11/ToInt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer_11
+{
+    class ToInt
+    {
+        int first, second;
+        bool a = false;
+
+        public void Identify(string oper){
+            for (int i = 0; a == true;i++){
+
+            }
+        }
+
+    }
+}
